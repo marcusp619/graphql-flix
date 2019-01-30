77de8f38330eb4657b268d1609234698a8c765fc
marcusp619 <marcusp619@gmail.com>
2019-01-29 23:31:25 -0700

    Update comments

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const { ApolloServer, gql } = require("apollo-server");
 const fetch = require("node-fetch");
 require("dotenv/config");
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
