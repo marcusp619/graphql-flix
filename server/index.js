4baa95aabd5f00f71c5334848117211e8a385949
marcusp619 <marcusp619@gmail.com>
2019-02-01 10:45:55 -0700

    Add sqlite db setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
-
+const { createStore } = require('./utils');
+const MovieAPI = require('./datasources/movie');
+const UserAPI = require('./datasources/user');
 const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
