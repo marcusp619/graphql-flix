d93d974030b679c10abf4e582e39725df72d0f82
marcusp619 <marcusp619@gmail.com>
2019-02-01 10:05:30 -0700

    Add schema for types

--- server/schema.js
+++ server/schema.js
@@ -1,5 +1,78 @@
 const { gql } = require('apollo-server');
 
-const typeDefs = gql``
+const typeDefs = gql`
+  type Query {
+    me: User
+    movies: [Movie]!
+    tv: [TV]
+    videos: [Videos]
+  }
+
+  type Mutation {
+    login(email: String): String
+
+    AddMovies(movieIds: [ID]!) MovieUpdateResponse!
+  }
+
+  type MovieUpdateResponse {
+    success: Boolean!
+    message: String
+    movies: [Movie]
+  }
+
+  # "MovieType" 
+  type Movie {
+    poster_path: String
+    adult: Boolean
+    overview: String
+    release_date: String
+    genre_ids: [String]
+    id: ID
+    original_title: String
+    original_language: String
+    title: String
+    backdrop_path: String
+    popularity: Float
+    vote_count: Float
+    videos: [Video]
+  }
+
+  # "VideoType"
+  type Video {
+    id: ID
+    iso_639_1: String
+    iso_3166_1: String
+    key: String
+    name: String
+    site: String
+    size: Int
+    type: String
+  }
+
+  # "TVType"
+  type TV {
+    poster_path: String
+    popularity: Float
+    id: ID
+    backdrop_path: String
+    vote_average: Float
+    overview: String
+    first_air_date: String
+    origin_country: [String]
+    genre_ids: [Int]
+    original_language: String
+    vote_count: Int
+    name: String
+    original_name: String
+  }
+
+  #"UserType"
+  type User {
+    id: ID!
+    email: String!
+    movies: [Movie]
+  }  
+`
 
 module.exports = typeDefs;
+
