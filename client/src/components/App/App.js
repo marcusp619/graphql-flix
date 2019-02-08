d69ce863899884e487a6a2dcbdd9a9b4bb8e8163
marcusp619 <marcusp619@gmail.com>
2019-02-08 12:53:53 -0700

    Remove component import from App component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "../Header";
 import BackgroundVideo from "../BackgroundVideo";
 import MovieList from "../MovieList";
