96248fec39ddaf0fe95bbae03d37d7a6c1c80f97
marcusp619 <marcusp619@gmail.com>
2019-02-28 08:44:57 -0700

    Add two tables in db

--- server/db/migrations/20190227153223_initial.js
+++ server/db/migrations/20190227153223_initial.js
@@ -1,8 +1,27 @@
-
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('users', table => {
+      table.increments('id').primary();
+      table.string('UserName');
+      table.string('UserEmail');
+
+      table.timestamps(true, true);
+    }),
+
+    knex.schema.createTable('content', table => {
+      table.increments('id').primary();
+      table.string('title');
+      table.integer('contentID').unsigned();
+      table.foreign('contentID').references('users.id');
+
+      table.timestamps(true, true);
+    })
+  ]);
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('content'),
+    knex.schema.dropTable('users')
+  ]);
 };
