f3df5e89b36d35ba3c4663eba146f4cd3f6da980
marcusp619 <marcusp619@gmail.com>
2019-03-03 12:44:24 -0700

    Add comment explaining RESTDataSource

--- server/src/datasources/movie.js
+++ server/src/datasources/movie.js
@@ -1,5 +1,8 @@
 const { RESTDataSource } = require("apollo-datasource-rest");
 
+/* The RESTDataSource sets up and in-memory cache that caches responses 
+from our REST resources with no additional setup. */
+
 class MovieAPI extends RESTDataSource {
   constructor() {
     super();
