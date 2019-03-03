c46aec8923c41ce30e8a17532e24f2291dcba45f
marcusp619 <marcusp619@gmail.com>
2019-03-03 13:58:04 -0700

    Add video class

---
+++ server/src/datasources/video.js
@@ -0,0 +1,34 @@
+const { RESTDataSource } = require("apollo-datasource-rest");
+require("dotenv").config();
+
+/* The RESTDataSource sets up and in-memory cache that caches responses 
+from our REST resources with no additional setup. */
+
+class VideoAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://api.themoviedb.org/3/movie/";
+  }
+
+  async getVideosById(videoId) {
+    const response = await this.get(
+      `${videoId}/videos?api_key=${process.env.API_KEY}&language=en-US`
+    );
+    return response.results.map(video => this.videoReducer(video));
+  }
+
+  videoReducer(video) {
+    return {
+      id: video.id,
+      iso_639_1: video.iso_639_1,
+      iso_3166_1: video.iso_3166_1,
+      key: video.key,
+      name: video.name,
+      site: video.site,
+      size: video.size,
+      type: video.type
+    };
+  }
+}
+
+module.exports = VideoAPI;
